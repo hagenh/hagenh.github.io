3dc7f5aeb8baf3ab6c1ac54181089ffb1d64e71a
hagenh <haakonhagen@live.no>
2022-05-04 19:21:12 +0200

    Initialize project using Create React App

---
+++ src/components/About.tsx
@@ -0,0 +1,7 @@
+const About = () => (
+  <div>
+    <div></div>
+  </div>
+);
+
+export default About;
---
+++ src/components/App.test.tsx
@@ -0,0 +1,13 @@
+import { MemoryRouter as Router } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
